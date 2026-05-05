bd9be4286d54dafb1f417d8939c14ba22db2ab15
sivakumar.M <sivakumarm2898@gmail.com>
2026-05-05 20:07:52 +0530

    Initial commit: Add Connectionstring

---
+++ TinyUrlApi/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace TinyUrlApi.Data
+{
+    //public class AppDbContext : DbContext
+    //{
+    //    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    //    public DbSet<UrlMapping> Urls { get; set; }
+    //}
+}
