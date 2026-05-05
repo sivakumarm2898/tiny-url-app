d0d945e327957ffb3a272a08f8d77c3c147a40b1
sivakumar.M <sivakumarm2898@gmail.com>
2026-05-05 15:14:15 +0530

    Initial commit with gitignore

---
+++ TinyUrlApi/Models/UrlMapping.cs
@@ -0,0 +1,13 @@
+﻿namespace TinyUrlApi.Models
+{
+    public class UrlMapping
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortCode { get; set; }
+        public bool IsPrivate { get; set; }
+        public int ClickCount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
